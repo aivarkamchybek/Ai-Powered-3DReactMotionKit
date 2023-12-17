110e14da1714ca820862f788e760f6a627e4b4e1
aivarkamchybek <127583391+aivarkamchybek@users.noreply.github.com>
2023-12-17 15:34:29 -0600

    updates

--- src/canvas/index.jsx
+++ src/canvas/index.jsx
@@ -7,11 +7,15 @@ import CameraRig from './CameraRig'
 
 const CanvasModel = () => {
   return (
-    <Canvas>
+    <Canvas
+    shadows
+    camera={{ position:[0, 0, 0], fov:30 }}
+    gl={{ preserveDrawingBuffer: true}}
+    className="w-full max-w-full h-full transition-all ease-in">
       <ambientLight intensity={0.5} />
       <Environment preset="city" />
       <CameraRig>
-        {/* <Backdrop /> */}
+        <Backdrop />
         <Center>
           <Shirt />
         </Center>
